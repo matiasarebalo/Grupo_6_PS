138da4d26922d04f655e8362f8726442d68146df
matiasarebalo <matiasarebalo@gmail.com>
2020-09-11 00:12:42 -0300

    login js

--- src/main/java/G6/PS/Ecommerce/controllers/LoginController.java
+++ src/main/java/G6/PS/Ecommerce/controllers/LoginController.java
@@ -26,12 +26,12 @@ public class LoginController {
 						@RequestParam(name="logout", required=false) String logout) {
 		model.addAttribute("error", error);
 		model.addAttribute("logout", logout);
-		return "user/login";		
+		return "/";		
     }
     
     @GetMapping("/logout")
 	public String logout(Model model) {
-		return "user/logout";
+		return "/";
     }
 	
 	@PostMapping("/loginsuccess")
